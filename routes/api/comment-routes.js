e0842933e1d1cfeffe4b7cb3997b68ab3d4b4b78
Tatum Stafford <tatumstafford1@gmail.com>
2021-02-11 19:00:07 -0600

    replies to comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controller');
 
 // POST add comment to /api/comments/:pizzaId
 router
     .route('/:pizzaId')
     .post(addComment);
 
-// DELETE comment to /api/comments/:pizzaId/:commentId
+// DELETE comment, PUT reply to /api/comments/:pizzaId/:commentId 
 router
     .route('/:pizzaId/:commentId')
+    .put(addReply)
     .delete(removeComment);
+
+// DELETE reply with id of individual reply
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
 
 module.exports = router;
